2a3c4c25629a0980abe9b45e7fd12589569553c8
ralmarcha <ralmarcha@gmail.com>
2023-03-09 10:44:08 +0100

    exercici2

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { useState } from "react";
 import {data} from './data'
 import './App.css';
-import { Checkbox , Label} from 'flowbite-react';
+import { Checkbox, Label , TextInput, Card} from 'flowbite-react';
 
 
 function App() {
 
   const [isChecked, setIsChecked] = useState(new Array(data.length).fill(false));
   const [total, setTotal] = useState(0);
-//fem un array dels q estan clicats
-  const handleOnChange = (position) => {
 
+  const handleOnChange = (position) => {
+  
     const checkedState = isChecked.map((item, index) =>
       index === position ? !item : item
     );
@@ -30,26 +30,51 @@ function App() {
 
     setTotal(total);
   };
-  
  
+  
+  const [mostrar, setMostrar] = useState(false);
+
+  const [idiomes, setIdiomes] = useState(0)
+  const [pagines, setPagines] = useState(0)
+  const suma = pagines * idiomes * 30 
 
   return (
    <>
-<div
-  className="flex flex-col gap-4"
-  id="checkbox"
->¿Qué vols fer?
+     <div  className="flex flex-col gap-4" id="checkbox" >
+        <h1>¿Qué vols fer?</h1>
 
-  <div className="flex items-center gap-2">
-    <Checkbox id="web" checked={isChecked[0]}
-          onChange={() =>handleOnChange(0)} />
-    <Label htmlFor="web">
-    Una pàgina web (500€)
-    </Label>
-  </div>
+     <div className="flex items-center gap-2">
+         <Checkbox id="web" checked={isChecked[0]}
+          onChange={() =>handleOnChange(0)} onClick={()=>setMostrar(!mostrar)}/>
+        <Label htmlFor="web">
+            Una pàgina web (500€)
+        </Label>
+         </div>
+            {mostrar ?
+                              
+         ( <div className="max-w-xs ml-10">
+          <Card>
+            <form className="flex flex-col gap-2">
+                <div className="flex gap-4">
+                  <div className="block">
+                 <Label htmlFor="pags" value="Número de pàgines :"/>
+                  </div>      
+                 <TextInput className="w-14" id="pags" type="number" required={true} name="pagines" onChange={ev => setPagines(ev.target.value)} placeholder="0"/>
+                </div>
+              <div className="flex gap-4">
+               <div className="block">
+                  <Label htmlFor="idiomes" value="Número d ' idiomes :"/>
+               </div>
+                 <TextInput className="w-14" id="idiomes" type="number" required={true} name="idiomes" onChange={ev => setIdiomes(ev.target.value)} placeholder="0"/>
+             </div>
+           </form>
+         </Card>
+          </div>)
+      : null}
+            
   <div className="flex items-center gap-2">
     <Checkbox id="seo" checked={isChecked[1]}
-          onChange={() =>handleOnChange(1)}/>
+          onChange={() =>handleOnChange(1)} />
     <Label htmlFor="seo">
     Una consultoria SEO (300€)
     </Label>
@@ -60,8 +85,9 @@ function App() {
     <Label htmlFor="ads">
     Una campanya de Google Ads  (200€)
     </Label>
-  </div>
-  <p>Preu: {total}€ </p> 
+        </div>
+        
+  <p>Preu: {total + suma}€ </p> 
       
 </div>
 
